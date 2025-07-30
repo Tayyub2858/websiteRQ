40880ce2cf139f3d7c2e809a148fc8bb55735219
tayyub rafique <tayyub2858@gmail.com>
2025-07-31 00:24:20 +0500

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://yourdomain.com',
+  generateRobotsTxt: true,
+  outDir: './public', // make sure sitemap goes into the public folder
+}
